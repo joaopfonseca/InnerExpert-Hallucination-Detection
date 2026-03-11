514b72bd6216b10ec4017405bbfd1d7d5df717d6
joaopfonseca <jpfonseca@novaims.unl.pt>
2026-03-11 15:05:29 +0000

    MNT update figure saving dirs

--- experiments/3.1-analyze-metrics.py
+++ experiments/3.1-analyze-metrics.py
@@ -19,8 +19,8 @@ from sklearn.preprocessing import StandardScaler
 model_slug = "allenai__OLMoE-1B-7B-0924-Instruct"
 dataset_slug = "realtimeqa-2026-02"
 
-data_dir = Path("data") / model_slug / dataset_slug
-figures_dir = Path("figures") / model_slug / dataset_slug / "3.1-analyze-metrics"
+data_dir = Path("data") / dataset_slug / model_slug
+figures_dir = Path("figures") / "3.1-analyze-metrics" / dataset_slug / model_slug
 figures_dir.mkdir(parents=True, exist_ok=True)
 
 df = pd.read_parquet(data_dir / "results.parquet")
