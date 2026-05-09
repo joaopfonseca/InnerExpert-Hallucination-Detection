b99eca40b984e4180b51c1b7718c4bff35765a41
joaopfonseca <jpfonseca@novaims.unl.pt>
2026-05-09 12:35:14 +0100

    FIX update monkey patch to newer transformers version

--- moeuncert/forwards/_model_forwards.py
+++ moeuncert/forwards/_model_forwards.py
@@ -24,6 +24,8 @@ def forward_olmoe(self, hidden_states):
     hidden_states = hidden_states.view(-1, hidden_dim)
     # router_logits: (batch * sequence_length, n_experts)
     router_logits = self.gate(hidden_states)
+    if isinstance(router_logits, tuple):
+        router_logits = router_logits[0]
 
     routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
     routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
